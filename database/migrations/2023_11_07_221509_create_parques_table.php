0210d67effce4e8d96c243af51fed5b52decd63b
roberto <robertoramirezmoreno@gmail.com>
2023-11-09 20:27:50 +0000

    aa

--- database/migrations/2023_11_07_221509_create_parques_table.php
+++ database/migrations/2023_11_07_221509_create_parques_table.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -7,19 +8,32 @@ class CreateParquesTable extends Migration
 {
     public function up()
     {
-        Schema::create('parques', function (Blueprint $table) {
-            $table->id();
-            $table->string('nombre');
-            $table->text('descripcion');
-            $table->string('calle');
-            $table->unsignedBigInteger('barrio_id');
-            $table->foreign('barrio_id')->references('id')->on('barrios');
-            $table->timestamps();
-        });
+        // Verificar si la tabla no existe antes de crearla
+        if (!Schema::hasTable('parques')) {
+            Schema::create('parques', function (Blueprint $table) {
+                $table->id();
+                $table->string('nombre', 100); // Longitud específica si es necesaria
+                $table->text('descripcion');
+                $table->string('calle', 100); // Longitud específica si es necesaria
+                $table->unsignedBigInteger('barrio_id');
+                $table->foreign('barrio_id')->references('id')->on('barrios');
+                $table->timestamps();
+            });
+        }
     }
 
     public function down()
     {
-        Schema::dropIfExists('parques');
+        // Verificar si la tabla existe antes de intentar eliminarla
+        if (Schema::hasTable('parques')) {
+            // Eliminar primero la clave foránea
+            Schema::table('parques', function (Blueprint $table) {
+                $table->dropForeign(['barrio_id']);
+            });
+
+            // Luego eliminar la tabla
+            Schema::dropIfExists('parques');
+        }
     }
 }
+
