aaf17f8a2bc61c0b7c72edc0a80d3b985226e5bb
roberto <robertoramirezmoreno@gmail.com>
2023-11-09 20:38:33 +0000

    aa

--- database/migrations/2023_11_07_221509_create_parques_table.php
+++ database/migrations/2023_11_07_221509_create_parques_table.php
@@ -8,32 +8,25 @@ class CreateParquesTable extends Migration
 {
     public function up()
     {
-        // Verificar si la tabla no existe antes de crearla
-        if (!Schema::hasTable('parques')) {
-            Schema::create('parques', function (Blueprint $table) {
-                $table->id();
-                $table->string('nombre', 100); // Longitud específica si es necesaria
-                $table->text('descripcion');
-                $table->string('calle', 100); // Longitud específica si es necesaria
-                $table->unsignedBigInteger('barrio_id');
-                $table->foreign('barrio_id')->references('id')->on('barrios');
-                $table->timestamps();
-            });
-        }
+        Schema::create('parques', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100); // Longitud específica si es necesaria
+            $table->text('descripcion');
+            $table->string('calle', 100); // Longitud específica si es necesaria
+            $table->unsignedBigInteger('barrio_id');
+            $table->foreign('barrio_id')->references('id')->on('barrios');
+            $table->timestamps();
+        });
     }
 
     public function down()
     {
-        // Verificar si la tabla existe antes de intentar eliminarla
-        if (Schema::hasTable('parques')) {
-            // Eliminar primero la clave foránea
-            Schema::table('parques', function (Blueprint $table) {
-                $table->dropForeign(['barrio_id']);
-            });
+        // Eliminar primero la clave foránea
+        Schema::table('parques', function (Blueprint $table) {
+            $table->dropForeign(['barrio_id']);
+        });
 
-            // Luego eliminar la tabla
-            Schema::dropIfExists('parques');
-        }
+        // Luego eliminar la tabla
+        Schema::dropIfExists('parques');
     }
 }
-
