740546a8cbc55b9aae4b32b374d06096cc6d0457
roberto <robertoramirezmoreno@gmail.com>
2023-11-09 20:09:10 +0000

    aa

--- database/migrations/2023_11_06_152254_create_barrios_table.php
+++ database/migrations/2023_11_06_152254_create_barrios_table.php
@@ -1,29 +1,26 @@
 <?php
 
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('barrios', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('body');
+            $table->text('body'); // Cambiado de string a text
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('barrios');
     }
 };
+
